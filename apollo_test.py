be7f562ec0b11932aaf4b5fd4f215f06005ecdfe
Sai Herng <39779745+saiherng@users.noreply.github.com>
2024-10-19 14:30:48 -0700

    Update apollo_test.py

--- apollo_test.py
+++ apollo_test.py
@@ -5,8 +5,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
-from gql import gql, Client
-from gql.transport.aiohttp import AIOHTTPTransport
+#from gql import gql, Client
+#from gql.transport.aiohttp import AIOHTTPTransport
 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
