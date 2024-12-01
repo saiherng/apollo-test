d3ab586f99de8f6aab506c2a17449ecdd9feaa06
Fabian Nino <git@fnino.com>
2024-12-01 02:24:03 -0800

    Added footer buttons unit tests

--- test_suite.py
+++ test_suite.py
@@ -12,6 +12,10 @@ import unittest
 from test_login_input import TestLoginInputUnitTest
 from test_space_explorer_page import TestSpaceExplorerHeaderUnitTest
 from test_launch_page import TestLauchPageUnitTest
+from test_home_button import TestHomeButtonUnitTest
+from test_cart_button import TestCartButtonUnitTest
+from test_profile_button import TestProfileButtonUnitTest
+from test_logout_button import TestLogoutButtonUnitTest
 # from test_login import TestLogin
 # from test_add_to_cart import TestAddToCart
 
@@ -26,6 +30,18 @@ def suite():
     
     # EC8-9: Testing of Launch Page
     test_suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestLauchPageUnitTest))
+    
+    # EC10: Testing of Footer Button Home
+    test_suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestHomeButtonUnitTest))
+    
+    # EC11: Testing of Footer Button Home
+    test_suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCartButtonUnitTest))
+    
+    # EC12: Testing of Footer Button Home
+    test_suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestProfileButtonUnitTest))
+    
+    # EC13: Testing of Footer Button Home
+    test_suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestLogoutButtonUnitTest))
     
     # test_suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestLogin))
     # test_suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestAddToCart))
---
+++ tests/unit_tests/test_profile_button.py
@@ -0,0 +1,50 @@
+import unittest
+
+from selenium.webdriver.common.by import By
+
+import sys
+sys.path.append('..')
+sys.path.append('../..')
+
+from ApolloDriver import ApolloSeleniumDriver
+from utils import printFail, printPass, TestInfo
+
+class TestProfileButtonUnitTest(unittest.TestCase):
+
+    def setUp(self):
+        # Setup the Chrome WebDriver
+        # Update with path
+        self.init_url = 'http://127.0.0.1:3000/launch/109'
+        self.driver = ApolloSeleniumDriver(self.init_url)
+    
+
+    def test_EC012_test_profile_button(self):  
+        testInfo = TestInfo("EC12", "Test Profile Button Navigation")
+
+        EXPECTED_URL = "http://127.0.0.1:3000/profile"
+        
+        self.driver.login("test@gmail.com")
+
+        try:
+            # Verify that the current url is the logged in one
+            self.assertEqual(self.driver.getCurrentUrl(), self.init_url)
+
+            # Get the link to test
+            profileLink = self.driver.find_element(By.XPATH, "/html/body/div/footer/div/a[3]")
+            
+            profileLink.click()
+
+            self.assertEqual(self.driver.getCurrentUrl(), EXPECTED_URL)
+
+            printPass(testInfo)
+        except:
+            printFail(testInfo)
+
+    
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
+
+
