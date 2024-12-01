f2a740d9a08e483d5560e3d552f5a9c9d5ecf032
Fabian Nino <git@fnino.com>
2024-11-30 22:11:01 -0800

    Added EC1-3: Unit testing the login input field for valid and invalid

--- test_suite.py
+++ test_suite.py
@@ -3,18 +3,24 @@ import os
 
 
 utils_path = os.path.abspath('tests')
+utils_path_unit = os.path.abspath('tests/unit_tests')
 sys.path.append(utils_path)
+sys.path.append(utils_path_unit)
 
 import unittest
 
-from test_login import TestLogin
-from test_add_to_cart import TestAddToCart
+from test_login_input import TestLoginInputUnitTest
+# from test_login import TestLogin
+# from test_add_to_cart import TestAddToCart
 
 def suite():
     test_suite = unittest.TestSuite()
+
+    # EC1-3: Testing of Login Input Field
+    test_suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestLoginInputUnitTest))
     
-    #test_suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestLogin))
-    test_suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestAddToCart))
+    # test_suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestLogin))
+    # test_suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestAddToCart))
 
 
     return test_suite
---
+++ tests/unit_tests/test_login_input.py
@@ -0,0 +1,76 @@
+import time
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import sys
+sys.path.append('..')
+sys.path.append('../..')
+
+from ApolloDriver import ApolloSeleniumDriver
+
+class TestLoginInputUnitTest(unittest.TestCase):
+    # Tests the input field for valid and invalid inputs
+
+    def setUp(self):
+        # Setup the Chrome WebDriver
+        # Update with path
+        self.driver = ApolloSeleniumDriver('http://127.0.0.1:3000')
+    
+    def _populateInputField(self, email):
+        # Returns userId
+
+        login_box = self.driver.find_element(By.CSS_SELECTOR, '[data-testid="login-input"]')
+
+        login_box.send_keys(email)
+
+        login_box.submit()
+
+        self.driver.driver.implicitly_wait(1)
+
+        return self.driver.getLocalStorage('userId')
+
+    def test_EC1_valid_email_login(self):  
+        print("\n")
+        print("EC1: Test Login input field with valid email address")
+
+        email = "testa@gmail.com"
+        
+        userId = self._populateInputField(email)
+
+        self.assertGreater(int(userId), 0)
+    
+    def test_EC2_invalid_email_login_string_only(self):
+        print("\n")
+        print("EC2: Test Login input field with invalid email address: String only")
+
+        email = "test"
+
+        userId = self._populateInputField(email)
+
+        self.assertIs(userId, None)
+    
+    def test_EC3_invalid_email_login_no_domain(self):
+        print("\n")
+        print("EC3: Test Login input field with invalid email address: no domain")
+
+        email = "test@"
+
+        userId = self._populateInputField(email)
+
+        self.assertIs(userId, None)
+
+    
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
+
+
