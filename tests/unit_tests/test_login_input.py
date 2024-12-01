2849d26d44e6dd2fb5b4b1215ff261ecbacd5aa0
Fabian Nino <git@fnino.com>
2024-11-30 22:17:04 -0800

    removed unused import

--- tests/unit_tests/test_login_input.py
+++ tests/unit_tests/test_login_input.py
@@ -1,13 +1,6 @@
-import time
 import unittest
 
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
-
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 import sys
 sys.path.append('..')
